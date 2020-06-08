0b0f172f1fd62e524a3a4d175602552a23d6fb5a
Lyudmila Chunihina <61645464+LyudMila123@users.noreply.github.com>
2020-06-08 13:07:31 +0300

    Update home_test.py
    "elif added"

--- home_test.py
+++ home_test.py
@@ -3,5 +3,7 @@ class Test():
     def test(self, a):
         if a in range(5, 67):
             return True
+        elif a in range(75, 83):
+            return Ok
         else:
             return False
